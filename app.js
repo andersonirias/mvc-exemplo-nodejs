0a45378587b855b0f7b86237b3a4a0ab4c845f5e
Anderson Irias <anderson.irias@gmail.com>
2019-03-08 22:22:34 -0300

    Atualização do arquivo APP

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-var app = require('./config/server');
+var express = require('express');
+var consign = require('consign');
+
+var app = express();
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+app.use(express.static('./src/public'));
+
+consign()
+  .include('src/routes')
+  .then('src/models')
+  .then('src/controllers')
+  .into(app);
 
 app.listen(3000, function(){
   console.log('APP rodando na porta 3000');
